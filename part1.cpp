7f3e26506677d7b2817d7c8d0f0a53e09c5bb809
Eleonora Reuk <106328644+EleonoraReuk@users.noreply.github.com>
2026-02-28 20:34:12 +0700

    Update part1.cpp

--- part1.cpp
+++ part1.cpp
@@ -1,13 +1,14 @@
-//#include <iostream>
+#include <iostream>
 
-//int main() {
-	//const int size = 10;
-	//int arr[size] = { 1, 7, 2, 4, 9, 0, 6, 4, 8, 3 };
-	//for (int i = 0; i < size; i++) {
-		//std::cout << arr[i];
-		//if (i < size - 1) {
-			//std::cout << ", ";
-		//}
-	//} return 0;
+int main() {
+	const int size = 10;
+	int arr[size] = { 1, 7, 2, 4, 9, 0, 6, 4, 8, 3 };
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i];
+		if (i < size - 1) {
+			std::cout << ", ";
+		}
+	} return 0;
 
-//}
+}
+
